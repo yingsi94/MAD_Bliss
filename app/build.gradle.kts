6ba1cc4dd03b87e93bad09b58133d70bce6f9e49
Selina Sia <selinasia@nana-mac.local>
2025-12-12 17:31:32 +0800

    Clean up duplicate Firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,8 +80,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-storage")
+
 
     // Additional from incoming branch
     implementation(libs.firebase.analytics)
